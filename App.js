3af5e13b3b45ccf9fd978e925a0646b025bcfc02
Denys Shourek <shourekdenis@gmail.com>
2019-05-26 18:09:58 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
